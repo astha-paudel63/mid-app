60787203e9a894578f7dd5c70a9704b8d0af2266
astha-paudel63 <53424026+astha-paudel63@users.noreply.github.com>
2022-04-22 09:13:43 +0545

    gif

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Counter.css";
+import logo from '../pic/hello.gif';
 
 const Counter = ({ initialValue, onChange }) => {
   // const [value, setValue] = useState(0);
@@ -7,28 +8,7 @@ const Counter = ({ initialValue, onChange }) => {
   // const [value, setValue] = useState(initialValue || 0);
   return (
     <div className="counter-wapper">
-      {/* <h1>counter</h1> */}
-      <div className="counter">
-      <div className="value-wapper">
-        <div className="value">
-          <h2>{initialValue}</h2>
-        </div>
-        </div>
-        <div className="count">
-        <div className="addd">
-          <button onClick={(e) => onChange(initialValue + 1)}>
-            <span>+</span>
-          </button>
-        </div>
-        
-        <div className="sub">
-          <button 
-          onClick={(e) => onChange(initialValue ? initialValue - 1 : initialValue)}>
-            <span>-</span>
-          </button>
-        </div>
-        </div>
-      </div>
+<img src={logo} alt="loading..." />
     </div>
   );
 };
